ac71d5bbb883ecdafeb069e52ce4a15f1063e788
Eric Schoonover <eric@spoon16.com>
2020-07-11 17:47:59 -0400

    make latest qmk_firmware compile again

--- keyboards/massdrop/ctrl/config.h
+++ keyboards/massdrop/ctrl/config.h
@@ -100,7 +100,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define LOCKING_RESYNC_ENABLE
 
 /* Force boot in NKRO mode */
-#define FORCE_NKRO
+//#define FORCE_NKRO
 
 /*
  * Feature disable options
@@ -108,9 +108,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /* disable debug print */
-#ifndef NO_DEBUG
-#  define NO_DEBUG
-#endif
+//#define NO_DEBUG
 
 /* disable print */
 //#define NO_PRINT
